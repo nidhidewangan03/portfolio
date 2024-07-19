0a96741c64aaf1b2f2f4bd3ca56ab79418985e09
NidhiContainovaId <nidhi@containova.com>
2024-07-19 15:24:09 +0530

    naviagtion page done for portfolio

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import About from './components/about';
+import About from './components/about/about';
+import Skill from './components/skill/skill';
+import Experience from './components/experience/experience';
+import Contact from './components/contact/contact';
 
 function App() {
   return (
-    <>  
+    <> 
       <Routes>     
-        <Route path= "/" element={<About/>}/>
-        <Route path= "/" element={<Skill/>}/>
-        <Route path= "/" element={<Experience/>}/>
-        <Route path= "/" element={<Contact/>}/>
+        <Route path="/" element={<About />} />
+        <Route path="/skill" element={<Skill />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import './About.scss';
-import { Routes } from 'react-router-dom';
-
+import React from 'react';
+import './about.scss';
+import Sidebar from '../sidebar/sidebar';
+import { Outlet } from 'react-router-dom';
 
 const About = () => {
   return (
-    <div>
-    hello
+    <div className='App'>
+      <Sidebar />
+      <div className='page'>
+        <span className='tags top-tags'>&lt;body&gt;</span>
+        <Outlet></Outlet>
+        <span className='tags bottom-tags'>
+          &lt;/body&gt;
+          <br/>
+          <span className='bottom-tag-html'>&lt;/html&gt;</span>
+        </span>
+      </div>
+
+      <div className='container home-page'>
+        <div className='text-zone'>
+          <h1>Hi, <br/>I'm</h1>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
