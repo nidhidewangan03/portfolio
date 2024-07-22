1c11b1b4fdb31a6e3e0ff4cc675aa63f8c799b15
NidhiContainovaId <nidhi@containova.com>
2024-07-22 16:58:58 +0530

    front page designed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 import About from './components/about/about';
 import Skill from './components/skill/skill';
 import Experience from './components/experience/experience';
 import Contact from './components/contact/contact';
+import Layout from './components/layout/layout';
+import Home from './components/home/home';
 
 function App() {
   return (
     <> 
       <Routes>     
-        <Route path="/" element={<About />} />
+        <Route path="/" element={<Layout/>} />
+          {/* <Route index element = {<Home/>}/> */}
+        <Route path="/about" element={<About />} />
         <Route path="/skill" element={<Skill />} />
         <Route path="/experience" element={<Experience />} />
         <Route path="/contact" element={<Contact />} />
--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
 import './about.scss';
-import Sidebar from '../sidebar/sidebar';
-import { Outlet } from 'react-router-dom';
-import Home from '../home/home';
 
 const About = () => {
   return (
-    <div className='App'>
-      <Sidebar />
-      <div className='page'>
-        <span className='tags top-tags'>&lt;body&gt;</span>
-        <Outlet></Outlet>
-        <span className='tags bottom-tags'>
-          &lt;/body&gt;
-          <br/>
-          <span className='bottom-tag-html'>&lt;/html&gt;</span>
-        </span>
-      </div>
-      <Home></Home>
+    <div>
+
     </div>
   );
 }
