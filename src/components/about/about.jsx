a66d27e3140350b940a4ff408e50ce5eac85bda2
NidhiContainovaId <nidhi@containova.com>
2026-01-20 00:55:12 +0530

    about page redesigned

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import './about.scss';
-import Image from '../../assests/about-img.png'
+import Image from '../../assests/about-img.png';
 
 const About = () => {
   return (
-    <div className ='container about-page'>
+    <div className='about-page'>
       <div className='text-zone'>
         <div id='about'>
           <h1>About Me</h1>
         </div>
-        <div  id='about-para'>
-          <p> I am a passionate learner with a keen interest in designing and frontend development.
-          As a beginner in UI/UX design, I am actively exploring the field. 
-          Currently, I work as a self-employed graphic designer, offering freelance services. 
-          I am also an active member of several vibrant tech communities, including MLSA, Lottie, GDSC SSTC, and HackClub SSTC.
+        <div id='about-para'>
+          <p>
+            I am a passionate learner with a keen interest in designing and frontend development.
+            As a beginner in UI/UX design, I am actively exploring the field. 
+            Currently, I work as a self-employed graphic designer, offering freelance services. 
+            I am also an active member of several vibrant tech communities, including MLSA, Lottie, GDSC SSTC, and HackClub SSTC.
           </p>
         </div>
-        <a className='flat-button' href="/NidhiDewangan_Resume.pdf" download>Download Resume</a>
+        <a className='flat-button' href="/NidhiDewangan_Resume.pdf" download>
+          Download Resume
+        </a>
       </div>
       <div id='about-img'>
-        <img src={Image} style={{ height: '80vh', width: '70vh', opacity:'1'}} alt="img-about" />
+        <img 
+          src={Image} 
+          alt="Designed Image"
+        />
       </div>
     </div>
   );
