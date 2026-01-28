8f330e05f1b4b3311d198672226571cc3190939a
NidhiContainovaId <nidhi@containova.com>
2026-01-28 16:39:17 +0530

    projrct page start to redesign

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -4,7 +4,14 @@ import './project.scss'
 const Project = () => {
   return (
     <div>
+    <div className='project-page'>
+      <div className='text-zone'>
+        <div id='project'>
+          <h1>Project</h1>
+        </div>
+      </div>
       
+    </div>     
     </div>
   )
 }
