7bd9eea89fa1ceee6b9cb8c09cb23292b7b4b84e
NidhiContainovaId <nidhi@containova.com>
2026-02-02 15:50:24 +0530

    my work section

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -1,19 +1,40 @@
 import React from 'react'
 import './project.scss'
+import DesignImage from '../../assests/hi.png'
+import DevelopmentImage from '../../assests/hi.png'
 
 const Project = () => {
   return (
-    <div>
     <div className='project-page'>
       <div className='text-zone'>
         <div id='project'>
-          <h1>Project</h1>
+          <h1>My Work</h1>
+        </div>
+        
+        <div id='project-para'>
+          <p>
+            I am currently working on several exciting projects that showcase my skills and creativity. 
+            Stay tuned for updates as I continue to develop and refine these projects!
+          </p>
+          <p>
+            From innovative web applications to creative design solutions, each project represents 
+            my dedication to quality and attention to detail.
+          </p>
         </div>
       </div>
       
-    </div>     
+      <div id='projects-container'>
+        <div className='design-projects'>
+          {/* <img src={DesignImage} alt="Designed Image"/> */}
+          <h1>Design Projects</h1>
+        </div>
+
+        <div className='development-projects'>
+          {/* <img src={DevelopmentImage} alt="Developed Image"/> */}
+          <h1>Development Projects</h1>
+        </div>
+      </div>
     </div>
   )
 }
-
 export default Project
